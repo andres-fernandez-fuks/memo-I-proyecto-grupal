838cd57ba78f8cd52d21292b783cbc339b10c30c
Diego <diego.balestieri1998@gmail.com>
2020-06-13 19:47:58 -0300

    agrego el run cucumber test

---
+++ src/test/java/pruebacucumber/RunCucumberTest.java
@@ -0,0 +1,6 @@
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;@RunWith(Cucumber.class)
+@CucumberOptions(strict = true, plugin = {"pretty"})
+public class RunCucumberTest{
+}
