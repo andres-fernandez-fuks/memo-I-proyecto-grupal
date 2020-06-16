8430a3ebd4b909c8a5d8a0ed254d8c6fd4533044
andyyy912 <48918488+andyyy912@users.noreply.github.com>
2020-06-16 12:59:04 -0300

    agrego test de get nombre de proyecto

---
+++ ModuloGestionDeProyectos/src/test/java/TestsProyecto/StepDefGetNombreDeProyecto.java
@@ -0,0 +1,28 @@
+package TestsProyecto;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import modelo.Proyecto;
+
+import static org.junit.Assert.assertEquals;
+
+
+public class StepDefGetNombreDeProyecto {
+    private String nombreOriginal;
+    private String nombreDevuelto;
+    private Proyecto proyecto;
+    @Given("un proyecto creado con cierto nombre {string}")
+    public void creoUnProyectoconNombre(String nombre) {
+        proyecto = new Proyecto(1,nombre);
+        nombreOriginal = nombre;
+    }
+
+    @When("pregunto el nombre del proyecto")
+    public void getNombreDeProyecto() { nombreDevuelto = proyecto.getNombre(); }
+
+    @Then("se me devuelve el nombre correcto")
+    public void elNombreDelProyectoEsCorrecto() { assertEquals(nombreOriginal,nombreDevuelto); }
+
+
+}
--- ModuloGestionDeProyectos/target/test-classes/TestsProyecto/testCrearProyecto.feature
+++ ModuloGestionDeProyectos/target/test-classes/TestsProyecto/testCrearProyecto.feature
@@ -4,3 +4,8 @@ Feature: Creacion de un proyecto
     Given un listado de proyectos vacio
     When creo un proyecto con nombre "ProyectoPrueba"
     Then el listado de proyectos pasa a tener un elemento.
+
+  Scenario: Creo un proyecto con un nombre, y lo pregunto
+    Given un proyecto creado con cierto nombre "Nuevo Proyecto"
+    When pregunto el nombre del proyecto
+    Then se me devuelve el nombre correcto
